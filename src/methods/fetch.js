b77d72ec1e42e1da148cb7c57f933acfc330fb0b
Leonardo Matos <leomp120894@gmail.com>
2019-07-30 18:13:57 -0300

    refactor(fetch): resolving with response data

--- src/methods/fetch.js
+++ src/methods/fetch.js
@@ -1,10 +1,30 @@
 import { search } from '@ecomplus/client'
 
 // request Search API and return promise
-const fetch = self => search({
+export default self => search({
   url: '/items.json',
   method: 'post',
   data: self.query
 })
+  // resolving with response data
+  .then(({ data }) => data)
 
-export default fetch
+/**
+ * @method
+ * @name EcomSearch#fetch
+ * @description Send POST request to
+ * [E-Com Plus Search API]{@link https://developers.e-com.plus/docs/api/#/search/items/items}
+ * and returns promise resolved with search result.
+ *
+ * @returns {Promise<result|error>}
+ *
+ * @example
+
+// Run search request
+search.fetch().then(result => {
+  console.log(result.took)
+  console.log(`{result.hits.total} items found:`)
+  console.log(result.hits.hits)
+})
+
+ */
