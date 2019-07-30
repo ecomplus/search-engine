ebd12ba83e9ace04d207a60ca1de2bddeb4b7a7d
Leonardo Matos <leomp120894@gmail.com>
2019-07-30 17:51:22 -0300

    feat(fetch): handling search api request with fetch method

--- src/methods/fetch.js
+++ src/methods/fetch.js
@@ -1,2 +1,10 @@
-export default self => new Promise((resolve, reject) => {
+import { search } from '@ecomplus/client'
+
+// request Search API and return promise
+const fetch = self => search({
+  url: '/items.json',
+  method: 'post',
+  data: self.query
 })
+
+export default fetch
