204eae22723fad8cbfdc7e340bc73a84e292d0bf
Leonardo Matos <leomp120894@gmail.com>
2019-08-01 19:26:27 -0300

    feat(set-search-term): handling 'setSearchTerm' method

--- src/methods/set-search-term.js
+++ src/methods/set-search-term.js
@@ -1,3 +1,56 @@
-export default (self, term) => {
+import { merge } from 'lodash'
 
+export default (self, term) => {
+  merge(self.dsl, {
+    // match name and/or keyword with term
+    // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
+    query: {
+      bool: {
+        must: {
+          multi_match: {
+            query: term,
+            fields: [
+              'name',
+              'keywords'
+            ]
+          }
+        }
+      }
+    },
+
+    // handle terms suggestion
+    // 'did you mean?'
+    // https://www.elastic.co/guide/en/elasticsearch/reference/current/search-suggesters.html
+    suggest: {
+      text: term,
+      words: {
+        term: {
+          field: 'name'
+        }
+      }
+    }
+  })
+
+  return self
 }
+
+/**
+ * @method
+ * @name EcomSearch#setSearchTerm
+ * @description Defines term to match with product name
+ * and/or keywords on next search request.
+ *
+ * @param {string} term - Term to be searched
+ * @returns {self}
+ *
+ * @example
+
+// Set new search term
+search.setSearchTerm('smartphone')
+
+* @example
+
+// Set new term and run search request
+search.setSearchTerm('notebook').fetch()
+
+ */
