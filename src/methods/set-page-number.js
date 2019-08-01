baca926c0b8f686ec4b63eb55ebc3035c611314e
Leonardo Matos <leomp120894@gmail.com>
2019-08-01 18:57:51 -0300

    feat(set-page-number): handling 'setPageNumber' method

--- src/methods/set-page-number.js
+++ src/methods/set-page-number.js
@@ -1,3 +1,26 @@
-export default (self, page) => {
-
+export default (self, page = 1) => {
+  self.dsl.from = self.dsl.size * (page - 1)
+  return self
 }
+
+/**
+ * @method
+ * @name EcomSearch#setPageNumber
+ * @description Defines next page number for
+ * search request (paginate results) starting from 1.
+ *
+ * @param {number} [page=1] - Integer page number
+ * @returns {self}
+ *
+ * @example
+
+// Configure to get second page of results and run search request
+search.setPageNumber(2).fetch()
+
+* @example
+
+// Set back to page number 1
+// It's the default preseted page number
+search.setPageNumber()
+
+ */
