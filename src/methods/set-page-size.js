cf12f36c73bf71894942c29f6fca5ef719ea3300
Leonardo Matos <leomp120894@gmail.com>
2019-08-01 13:48:44 -0300

    feat(set-page-size): handing 'setPageSize' method

--- src/methods/set-page-size.js
+++ src/methods/set-page-size.js
@@ -1,4 +1,25 @@
-export default (self, limit) => {
+export default (self, limit = 24) => {
   self.query.size = limit
   return self
 }
+
+/**
+ * @method
+ * @name EcomSearch#setPageSize
+ * @description Defines maximum number of result items for next search request.
+ *
+ * @param {number} [limit=24] - Integer max result size
+ * @returns {self}
+ *
+ * @example
+
+// Set page size 20 and run search request
+search.setPageSize(20).fetch()
+
+* @example
+
+// Set to 24 the maxium results for next search
+// It's the default preseted page size
+search.setPageSize()
+
+ */
