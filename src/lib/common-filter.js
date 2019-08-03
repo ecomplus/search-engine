710db433ed4927e6042054c5dcbace617efed359
Leonardo Matos <leomp120894@gmail.com>
2019-08-03 17:13:22 -0300

    chore(lib): function to handle common filters

---
+++ src/lib/common-filter.js
@@ -0,0 +1,22 @@
+export default (self, field, prop, value) => {
+  // abstraction to set or remove filter by field IDs/names
+  const newProp = field !== null ? `${field}.${prop}` : prop
+  if (Array.isArray(value)) {
+    // setup new filter object
+    const filter = { terms: {} }
+    filter.terms[newProp] = value
+    // remove concurrent filter and merge the new
+    let concurrentProp
+    if (field !== null) {
+      concurrentProp = `${field}.${(prop === '_id' ? 'name' : '_id')}`
+    } else {
+      // product SKUs or IDs
+      concurrentProp = prop === '_id' ? 'sku' : '_id'
+    }
+    return self.removeFilter(concurrentProp).mergeFilter(filter)
+  } else if (value === null) {
+    // just remove respective filter
+    return self.removeFilter(newProp)
+  }
+  return self
+}
