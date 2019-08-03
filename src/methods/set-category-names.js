bca22b0241102e17c43a6c0370ff133f5d52cbb9
Leonardo Matos <leomp120894@gmail.com>
2019-08-03 17:41:42 -0300

    feat(set-category-names): handling 'setCategoryNames' method

---
+++ src/methods/set-category-names.js
@@ -0,0 +1,25 @@
+import commonFilter from './../lib/common-filter'
+
+// handle filter by category names
+export default (self, categoryNames) => commonFilter(self, 'categories', 'name', categoryNames)
+
+/**
+ * @method
+ * @name EcomSearch#setCategoryNames
+ * @description Defines list of category names to match on next search request,
+ * filtering product results by category.
+ *
+ * @param {array<string>|null} categoryNames - List of category names
+ * @returns {self}
+ *
+ * @example
+
+// Set filter by categories and run search request
+search.setCategoryNames([ 'Headset', 'Monitores Gamer' ]).fetch()
+
+ * @example
+
+// Remove filter by category names
+search.setCategoryNames(null)
+
+ */
