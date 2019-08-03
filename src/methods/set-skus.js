13172b2ea538b29bb70b286bce7d6d349e1b7c59
Leonardo Matos <leomp120894@gmail.com>
2019-08-03 17:28:14 -0300

    feat(set-skus): handling 'setSkus' method

---
+++ src/methods/set-skus.js
@@ -0,0 +1,24 @@
+import commonFilter from './../lib/common-filter'
+
+// handle filter by product SKUs
+export default (self, skus) => commonFilter(self, null, 'sku', skus)
+
+/**
+ * @method
+ * @name EcomSearch#setProductIds
+ * @description Defines list of product SKUs to match on next search request.
+ *
+ * @param {array<string>|null} skus - List of product SKUs
+ * @returns {self}
+ *
+ * @example
+
+// Set filter by SKUs for next search
+search.setSkus(['nb-apl-1203', 'hd-csr-303'])
+
+ * @example
+
+// Remove filter by SKU and run search request
+search.setSkus(null).fetch()
+
+ */
