f7754821676d920dbfde903c3927d049a258e546
Leonardo Matos <leomp120894@gmail.com>
2019-08-03 17:35:48 -0300

    feat(set-category-ids): handling 'setCategoryIds' method

---
+++ src/methods/set-category-ids.js
@@ -0,0 +1,25 @@
+import commonFilter from './../lib/common-filter'
+
+// handle filter by category IDs
+export default (self, categoryIds) => commonFilter(self, 'categories', '_id', categoryIds)
+
+/**
+ * @method
+ * @name EcomSearch#setCategoryIds
+ * @description Defines list of category IDs to match on next search request,
+ * filtering product results by category.
+ *
+ * @param {array<string>|null} categoryIds - List of category ObjectIDs (`_id`)
+ * @returns {self}
+ *
+ * @example
+
+// Set filter by category IDs for next search
+search.setCategoryIds([ '5c7009fdc626be23430d4f82', '5c700a53c626be23430d4f88' ])
+
+ * @example
+
+// Remove filter by category IDs and run search request
+search.setCategoryIds(null).fetch()
+
+ */
